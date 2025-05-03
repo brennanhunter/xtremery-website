3173ed32ba7b0c0dd2f4b42de4cac8f9d9ac61ef
Hunter Coleman <40704537+brennanhunter@users.noreply.github.com>
2025-05-03 14:37:13 -0400

    removed unused import for ESLint.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 import Header from '../components/Header';
-import Image from 'next/image';
 import HonorGrid from '../components/HonorGrid';
 import WhoAmI from '../components/WhoAmI';
 
