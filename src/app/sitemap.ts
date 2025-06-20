570a32355ce0ab6f516d8ebdd74ddc36a49628c7
Hunter Coleman <40704537+brennanhunter@users.noreply.github.com>
2025-06-19 23:48:27 -0400

    sitemapping blog.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -34,5 +34,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.7,
     },
+    {
+  url: `${baseUrl}/blog`,
+  lastModified: new Date(),
+  changeFrequency: 'weekly',
+  priority: 0.8,
+},
   ]
 }
+
