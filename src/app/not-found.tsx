e31d89a876b90007e91a7cf8362134c46750cf03
Hunter Coleman <40704537+brennanhunter@users.noreply.github.com>
2025-06-19 23:28:49 -0400

    ...

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@ export default function NotFound() {
   return (
     <div>
       <h2>Not Found</h2>
-      <p>Could not find the requested resource.</p>
+      <p>Could not find the requested resource!</p>
     </div>
   );
 }
