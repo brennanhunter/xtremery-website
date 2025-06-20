4f647983fda5018b726763a9f39177e9cd4b6f53
Hunter Coleman <40704537+brennanhunter@users.noreply.github.com>
2025-06-19 22:44:14 -0400

    ESLint.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 export default function NotFound() {
   return (
     <html lang="en">
@@ -7,10 +9,10 @@ export default function NotFound() {
       <body>
         <div style={{ padding: '2rem', textAlign: 'center' }}>
           <h1>404 - Page Not Found</h1>
-          <p>The page you're looking for doesn't exist.</p>
-          <a href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
+          <p>The page you&apos;re looking for doesn&apos;t exist.</p>
+          <Link href="/" style={{ color: 'blue', textDecoration: 'underline' }}>
             Go back home
-          </a>
+          </Link>
         </div>
       </body>
     </html>
