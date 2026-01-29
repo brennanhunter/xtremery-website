e936ddf05ba9898b4b3066bbbc97bab595bf4977
Hunter Coleman <hunter.coleman@scp.cy>
2026-01-28 23:00:14 -0500

    Updated 400 bad request issues.

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,19 @@
 import type { NextConfig } from "next";
 
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
-    domains: ['cdn.sanity.io'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        pathname: '/**',
+      },
+    ],
+    formats: ['image/avif', 'image/webp'],
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+    minimumCacheTTL: 60,
   },
-}
+};
 
-module.exports = nextConfig
+export default nextConfig;
